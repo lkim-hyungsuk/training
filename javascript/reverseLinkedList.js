19131907ea55bd3c3959afcbb5a7501fd92afcbc
lkim-hyungsuk <114844493+lkim-hyungsuk@users.noreply.github.com>
2022-10-02 22:57:24 +0900

    (add) reverseLinkedList

---
+++ javascript/reverseLinkedList.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {debugger;
+  let prev, next;
+  
+  // just focus on the current head only, don't think what to do with next one
+  while (head) {
+      next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+  }
+                                
+  return prev;
+};
+
+function ListNode(val, next) {
+this.val = (val===undefined ? 0 : val)
+this.next = (next===undefined ? null : next)
+}
+
+let head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+reverseList(head);
